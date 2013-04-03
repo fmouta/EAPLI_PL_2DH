e477ab5ec5f4caba997897408db0451f44b90a25
1110422-PauloRodrigues <1110422@isep.ipp.pt>
2013-04-03 16:39:22 +0100

    Adicionada Classe Income

---
+++ eapli.ExpenseManager/src/Model/Income.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author i110422
+ */
+public class Income {
+    
+}
