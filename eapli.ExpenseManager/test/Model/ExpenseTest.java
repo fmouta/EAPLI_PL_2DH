567424f953fd6af66c5610d94558e49817c9503c
Mario Garrido <skinie18@gmail.com>
2013-04-09 17:47:59 +0100

    Começar a fazer os testes unitarios das classes Expense e ExpenseType #1111623

---
+++ eapli.ExpenseManager/test/Model/ExpenseTest.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author i111623
+ */
+public class ExpenseTest {
+    
+    public ExpenseTest() {
+    }
+    
+    @Test
+    public void testGetDate() {
+        System.out.println("getDate");
+        fail("The test case is a prototype.");
+    }
+
+    @Test
+    public void testGetAmount() {
+        System.out.println("getAmount");
+        fail("The test case is a prototype.");
+    }
+
+    @Test
+    public void testGetDescription() {
+        System.out.println("getDescription");
+        fail("The test case is a prototype.");
+    }
+}
